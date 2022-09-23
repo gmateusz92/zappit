8a4b36970445af8b6f442857f4a07c8944233b76
mateusz <matek1600@o2.pl>
2022-09-23 11:19:32 +0200

    1

---
+++ posts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
+
+class PostList(generics.ListAPIViews): # wyswietla wszyskie posty
+    queryset = Post.objects.all( )#queryset = to co chcemy wswietlac na koncu
+    serializer_class = PostSerializer
